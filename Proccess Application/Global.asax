196fd849fd28d03d14555fb9b8648523c252c631
Joel <jgimbel@live.com>
2014-04-29 11:26:06 -0500

    initial commit

---
+++ Proccess Application/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proccess_Application.MvcApplication" Language="C#" %>
---
+++ Proccess Application/Models/IndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace App.Models.Index
+{
+    public class IndexModel
+    {
+        public Process[] processes { get; set; }
+	}
+}
